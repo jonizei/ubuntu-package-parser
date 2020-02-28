9da3454f653e817822672edbecaa6bd043cb1241
jonizei <joni.koskinen@cs.tamk.fi>
2020-02-28 15:07:08 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Package parser',
+    version='1.0',
+    description='Ubuntu/Debian status file parser',
+    author='Joni Koskinen',
+    author_email='joni.m.koskinen@gmail.com',
+    packages=['src'],
+    install_requires['http.server', 'json']
+)
