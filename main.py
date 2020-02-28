51a76eb32cc01c58784a5ac4f8d56511661d9e95
jonizei <joni.koskinen@tuni.fi>
2020-02-28 03:18:45 +0200

    Prints packages to a web page

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ hostName = "localhost"
 hostPort = 8080
 filePath = "/var/lib/dpkg/status"
 
+def get_package_names(package_list):
+    names = []
+
+    for package in package_list:
+        names.append(package["name"])
+
+    return names
+
+def get_package_by_name(name, package_list):
+
+    package = {}
+
+    for pkg in package_list:
+        if pkg["name"] == name:
+            package = pkg
+            break
+
+    return package
+
 def main():
 
     package_list = parser.parse(filePath)
@@ -19,23 +38,27 @@ def main():
                 self.send_response(200)
                 self.send_header("Content-Type", "application/json")
                 self.end_headers()
-                data = json.dumps(package_list)
+
+                data = json.dumps(get_package_names(package_list))
                 self.wfile.write(data.encode("utf-8"))
 
-            elif self.path == "/jquery-3.4.1.min.js":
+            elif self.path.startswith("/packages/"):
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
+                self.end_headers()
+                
+                package_name = self.path.replace("/packages/", "")
+                package = get_package_by_name(package_name, package_list)
+                data = json.dumps(package)
+
+                self.wfile.write(data.encode("utf-8"))
+
+            elif self.path == "/vue.js":
                 self.send_response(200)
                 self.send_header("Content-Type", "text/javascript")
                 self.end_headers()
 
-                f = open("jquery-3.4.1.min.js", "r")
-                self.wfile.write(f.read().encode("utf-8"))
-
-            elif self.path == "/index.css":
-                self.send_response(200)
-                self.send_header("Content-Type", "text/css")
-                self.end_headers()
-
-                f = open("index.css", "r")
+                f = open("vue.js", "r")
                 self.wfile.write(f.read().encode("utf-8"))
 
             else:
